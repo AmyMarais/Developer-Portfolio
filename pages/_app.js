08549b432c0c7bac519578cb68e7a8bb98f4ab8e
Amy Marais <amy.marais99@gmail.com>
2021-06-27 23:43:05 +0200

    make it better

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,25 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 import Layout from "../components/Layout";
 
-function MyApp ( { Component, pageProps}) {
-    return ( 
-        <Layout>
-            <Component {...pageProps}/> {/*here the pages are output*/}
-        </Layout>
-     );
+const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return (
+  <Layout>
+      <Component {...pageProps} /> {/*here the pages are output*/}
+      </Layout>
+  );
 }
- 
-export default MyApp;
+
+export default App
